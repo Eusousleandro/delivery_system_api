ddee030410a68726c8be1775f26fef343a66dbff
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-20 19:43:18 -0300

    Implementation and modified in models restaurant.

--- app/infrastructure/database/models/restaurant.py
+++ app/infrastructure/database/models/restaurant.py
@@ -1,2 +1,12 @@
-class Restaurant:
-    pass
+from sqlalchemy import Column, Integer, String
+
+from app.infrastructure.database.connection import Base
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(100), unique=True, index=True)
+    cnpj = Column(String(20), unique=True, index=True, nullable=False)
+    password = Column(String(50), nullable=False)
