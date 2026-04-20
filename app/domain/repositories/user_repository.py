945fd3106d1dae9c8b6f7f49dedc3f6dc0cfc84b
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-20 18:19:26 -0300

    Refactoring the user repository.

--- app/domain/repositories/user_repository.py
+++ app/domain/repositories/user_repository.py
@@ -1,26 +1,26 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional
 
 from app.domain.entities.user import User
 
 class IUserRepository(ABC):
 
     @abstractmethod
-    def get_all(self, user: User) -> List[User]:
+    def get_all(self) -> List[User]:
         pass
 
     @abstractmethod
-    def get_user_id(self, user: User):
+    def get_user_id(self, id: int) -> Optional[User]:
         pass
 
     @abstractmethod
-    def create_user(self, user: User):
+    def create(self, user: User) -> None:
         pass
 
     @abstractmethod
-    def update_user(self, user: User):
+    def update(self, user: User) -> None:
         pass
 
     @abstractmethod
-    def delete_user(self, user: User):
+    def delete(self, id: int) -> None:
         pass
