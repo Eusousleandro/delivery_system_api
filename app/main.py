28d0d12ec1ab4d2a7afb9d40c57caf8748a7e937
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-21 17:41:48 -0300

    Refactoring the main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
+from app.infrastructure.database.connection import engine, Base
 from app.interfaces.api.user_controller import router as user_router
 from app.interfaces.handler.exceptions_handler import register_exception_handlers
 
 app = FastAPI()
-
+Base.metadata.create_all(bind=engine)
 register_exception_handlers(app)
 app.include_router(user_router)
