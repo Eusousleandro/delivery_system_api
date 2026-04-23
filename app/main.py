f55327a1cbb301b7541a6d7b9e10abe04648bc26
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-23 02:09:27 -0300

    Refactoring the code of import of main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
 from app.infrastructure.database.connection import engine, Base
-from app.interfaces.api.controllers.login_controller import router as login_router
-from app.interfaces.api.controllers.user_controller import router as user_router
+from app.interfaces.api.login_controller import router as login_router
+from app.interfaces.api.user_controller import router as user_router
 from app.interfaces.handler.exceptions_handler import register_exception_handlers
 
 app = FastAPI()
